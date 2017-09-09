8f82bbcd4f0fbfc654e451c2e9dc63c397d2a694
Chase Stewart <31610091+ChaseStewart@users.noreply.github.com>
2017-09-08 20:18:42 -0600

    Added copyright, bit better brief description, update to 1.1

--- data_structs/src/double_linklist.c
+++ data_structs/src/double_linklist.c
@@ -1,20 +1,21 @@
 /*****************************************************
- * Copyright (C) 2017 by Chase E Stewart
- *
- * 
- *
- *
- *
+ * Redistribution, modification or use of this software in source or binary forms 
+ * is permitted as long as the files maintain this copyright. Users are permitted to 
+ * modify this and use it to learn about the field of embedded software but don't copy 
+ * my (Chase E Stewart's) work for class, I worked really hard on this. Alex Fosdick and 
+ * the University of Colorado and Chase E Stewart are not liable for any misuse of this material. 
+ * License copyright (C) 2017 originally from Alex Fosdick, code by Chase E Stewart.
  *****************************************************/
 /*
  * @file double_linklist.c
  * @brief A simple library for a doubly-linked list
  * 
- * This circular buffer holds structs 
+ * This circular buffer holds a buffer and helpful counts/pointers in a struct
+ * The file also contains methods to create, destroy, add, remove, dump, and count the struct.
  *
  * @author Chase E Stewart
- * @date Sept 4 2017
- * @version 1.0
+ * @date Sept 8 2017
+ * @version 1.1
  *
  */
 #include <stdint.h>
