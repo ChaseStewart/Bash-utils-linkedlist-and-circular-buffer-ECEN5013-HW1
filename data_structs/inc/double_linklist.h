898c4c523cacf13a3d7881294c7251b1cabb9e2c
Chase Stewart <31610091+ChaseStewart@users.noreply.github.com>
2017-09-08 20:24:48 -0600

    update license, readme, etc

--- data_structs/inc/double_linklist.h
+++ data_structs/inc/double_linklist.h
@@ -1,20 +1,22 @@
 /*****************************************************
- * Copyright (C) 2017 by Chase E Stewart
- *
- * 
- *
- *
- *
+ * Redistribution, modification or use of this software in source or binary forms 
+ * is permitted as long as the files maintain this copyright. Users are permitted to 
+ * modify this and use it to learn about the field of embedded software but don't copy 
+ * my (Chase E Stewart's) work for class, I worked really hard on this. Alex Fosdick and 
+ * the University of Colorado and Chase E Stewart are not liable for any misuse of this material. 
+ * License copyright (C) 2017 originally from Alex Fosdick, code by Chase E Stewart.
  *****************************************************/
 /**
  * @file double_linklist.h
  * @brief A simple library for a doubly-linked list
  * 
- * This circular buffer holds structs 
+ * This linked list is a struct of structs that creates a doubly-linked list. There is no master struct here;
+ * all operations are completed by iterating through the list.
+ * The file also contains methods to add, remove, search, destroy, and count the linked list, one node at a time.
  *
  * @author Chase E Stewart
- * @date Sept 4 2017
- * @version 1.0
+ * @date Sept 8 2017
+ * @version 1.1
  *
  */
 #ifndef __MY_DBL_LINKLIST_H__
@@ -97,6 +99,7 @@ dbl_ll_status_t search_dbl_ll(struct dbl_ll_node **ptr, uint32_t *retval, uint32
  *
  *
  * @param struct dbl_ll_node **ptr: pointer to a pointer to the head dbl_ll node
+ * @param uint32_t *retval: pointer to the returned size of the linked list
  *
  * @return dbl_ll_status_t: an enum of the function exit status
  */
