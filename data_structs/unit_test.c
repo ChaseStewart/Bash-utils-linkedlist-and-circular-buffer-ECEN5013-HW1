3ef1aaf35281918308bd9ca4914a1aa8ce750425
Chase Stewart <31610091+ChaseStewart@users.noreply.github.com>
2017-09-19 22:02:23 -0600

    Comment unit_test.c

--- data_structs/unit_test.c
+++ data_structs/unit_test.c
@@ -1,17 +1,56 @@
+/*****************************************************
+ * Redistribution, modification or use of this software in source or binary forms 
+ * is permitted as long as the files maintain this copyright. Users are permitted to 
+ * modify this and use it to learn about the field of embedded software but don't copy 
+ * my (Chase E Stewart's) work for class, I worked really hard on this. Alex Fosdick and 
+ * the University of Colorado and Chase E Stewart are not liable for any misuse of this material. 
+ * License copyright (C) 2017 originally from Alex Fosdick, code by Chase E Stewart.
+ *****************************************************/
+/**
+ * @file unit_test.c
+ * @brief A unit test program for the data structures from the last assignment
+ * 
+ * This runs the testCircBuff and the testDoubleLinklist tests using the Unity Framework
+ *
+ * @author Chase E Stewart
+ * @date Sept 19 2017
+ * @version 1.0
+ *
+ */
+
 #include "unity.h"
 #include "test/testCircBuff.h"
 #include "test/testDoubleLinklist.h"
 
-
+/*
+ * @brief just a stub for test setup
+ *
+ * @param none
+ *
+ * @return none
+ */
 void setUp(void)
 {
 }
 
+/*
+ * @brief just a stub for test teardown
+ *
+ * @param none
+ *
+ * @return none
+ */
 void tearDown(void)
 {
 }
 
-
+/*
+ * @brief start unity, run all unit tests, return results to stdout
+ *
+ * @param none
+ *
+ * @return int status 
+ */
 int main(void)
 {
 	/* Start the unit testing */
