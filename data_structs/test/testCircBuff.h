e645ee0dd8b377229243db99bee05f1d6d5dd05c
Chase Stewart <31610091+ChaseStewart@users.noreply.github.com>
2017-09-19 22:27:23 -0600

    Update testCircBuff.h

--- data_structs/test/testCircBuff.h
+++ data_structs/test/testCircBuff.h
@@ -7,34 +7,80 @@
  * License copyright (C) 2017 originally from Alex Fosdick, code by Chase E Stewart.
  *****************************************************/
 /**
- * @file testDoubleLinklist.h
- * @brief A set of unit tests for the doubleLinklist from HW 
+ * @file testCircBuff.h
+ * @brief A set of unit tests for the circular buffer from HW 1
  * 
- * This linked list is a struct of structs that creates a doubly-linked list. There is no master struct here;
- * all operations are completed by iterating through the list.
- * The file also contains methods to add, remove, search, destroy, and count the linked list, one node at a time.
  *
  * @author Chase E Stewart
- * @date Sept 13 2017
- * @version 1.1
+ * @date Sept 19 2017
+ * @version 1.0
  *
  */
 
 #ifndef __MY_UNIT_CIRC_BUFF_H__
 #define __MY_UNIT_CIRC_BUFF_H__
 
+/*
+ * @brief test alloc_c_buff
+ *
+ * @param none
+ *
+ * @return none
+ */
 void test_allocate_c_buff(void);
 
+/*
+ * @brief test dump_c_buff
+ *
+ * @param none
+ *
+ * @return none
+ */
 void test_dump_c_buff(void);
 
+/*
+ * @brief test destroy_c_buff
+ *
+ * @param none
+ *
+ * @return none
+ */
 void test_destroy_c_buff(void);
 
+/*
+ * @brief test add_c_buff
+ *
+ * @param none
+ *
+ * @return none
+ */
 void test_add_c_buff(void);
 
+/*
+ * @brief test size_c_buff
+ *
+ * @param none
+ *
+ * @return none
+ */
 void test_size_c_buff(void);
 
+/*
+ * @brief test remove_c_buff
+ *
+ * @param none
+ *
+ * @return none
+ */
 void test_remove_c_buff(void);
 
+/*
+ * @brief test c_buff full/empty
+ *
+ * @param none
+ *
+ * @return none
+ */
 void test_c_buff_full_and_empty(void);
 
 #endif /* end __MY_UNIT_CIRC_BUFF_H__ */
